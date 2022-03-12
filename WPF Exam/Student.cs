7c4fdbc79244ec64718b89b9879ca4bb1f11c8b2
Yakushev Roman <boxboxbox@inbox.ru>
2022-03-12 10:31:21 +0300

    Add Class Student

---
+++ WPF Exam/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Exam
+{
+    internal class Student
+    {
+        string LastName { get; set; }
+        string Groupe { get; set; }
+        string Teacher { get; set; }    
+        double AvgScore { get; set; }
+    }
+}

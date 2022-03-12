53fb5f54737f2782f5812558ffcfaf52223e1141
Yakushev Roman <boxboxbox@inbox.ru>
2022-03-12 11:05:00 +0300

    Add MainWindows

--- WPF Exam/StudyDB.cs
+++ WPF Exam/StudyDB.cs
@@ -12,16 +12,7 @@ namespace WPF_Exam
         {
         }
 
-        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
-        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
-
         public virtual DbSet<Student> Students { get; set; }
         public virtual DbSet<Teacher> Teachers { get; set; }
     }
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
---
+++ WPF Exam/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF_Exam
+{
+    internal class ViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SelectionChangedEventArgs SelectionChanged;
+        public void PropertyChanging(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        List<Student> Students;
+        List<Teacher> Teachers;
+
+
+    }
+}
